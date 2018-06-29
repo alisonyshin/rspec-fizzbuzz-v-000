a26c1091673a0df7d856b31eb628b33a90642722
Alison Shin <alisonyshin@gmail.com>
2018-06-29 07:27:49 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,7 +6,7 @@ def fizzbuzz(input)
     "Fizz"
   elsif input % 5 == 0 
     "Buzz"
-  elsif input % 3 == 0 && input % 5 == 0 
+  elsif (input % 3 == 0) && (input % 5 == 0) 
     "FizzBuzz"
   end
 end
